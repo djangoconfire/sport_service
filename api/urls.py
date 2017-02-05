6a9a0bc610f92d3d5d6521df726c7bc80686409a
djangoconfire <djangopycon@gmail.com>
2017-02-06 02:28:56 +0530

    modify urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from api import views
 urlpatterns = format_suffix_patterns([
     url(r'^fetch/$', views.BuddyList.as_view(), name='buddy-list'),
     url(r'^assign/$', views.BuddyPost.as_view(), name='buddy-assign'),
-    url(r'^(?P<buddy_id>[\d+])/disable/$',views.disable,name="buddy-disable"),
+    # url(r'^(?P<buddy_id>[\d+])/disable/$',views.disable,name="buddy-disable"),
     url(r'^(?P<buddy_id>[\d+])/detail/$', views.BuddyDetail.as_view(), name='buddy-detail'),
     url(r'^post/$',views.buddy_list,name="buddy-post"),
 ])
